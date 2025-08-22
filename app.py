c2a152c21aa4822bb04c6937e1653412e179c97d
Ranjith <its.ranjith95@gmail.com>
2025-08-22 21:01:38 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
+import json
 import pickle
-from flask import Flask, request, jsonify, render_template
+
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
+import pandas as pd
 
-app = Flask(__name__)
-
-# Load the pre-trained model and scaler
-regmodel = pickle.load(open('regmodel.pkl', 'rb'))
-scaler = pickle.load(open('scaling.pkl', 'rb'))
-
+app=Flask(__name__)
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api', methods=['POST'])
+
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
-    data = request.json['data']   # expecting JSON {"data": {...}}
-    print("Input Data:", data)
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
 
-    # Convert input dictionary to numpy array
-    new_data = np.array(list(data.values())).reshape(1, -1)
 
-    # Use the pre-fitted scaler
-    scaled_data = scaler.transform(new_data)
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template('home.html', prediction_text='The House price prediction is {}'.format(output))
 
-    # Predict using the regression model
-    output = regmodel.predict(scaled_data)
-    print("Prediction:", output[0])
 
-    return jsonify({"prediction": float(output[0])})
-
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
+    
